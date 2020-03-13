d8ea128b6252e9794210fde0f7bb234137493f76
vladFedurko <fedurko.v@gmail.com>
2020-03-13 22:33:46 +0300

    ADD: simple server without any interface and authentication

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,5 +1,43 @@
-public class MainServer {
-    public static void main(String args[]) {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
+public class MainServer {
+    private static final int SERVER_PORT = 4567;
+
+    public static void main(String[] args) {
+        ArrayList<String> ips = new ArrayList<>(5);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final ServerSocket serverSocket =
+                            new ServerSocket(SERVER_PORT);
+                    while(!Thread.interrupted()){
+                        Socket socket = serverSocket.accept();
+                        DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+                        DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+                        String userIp = inputStream.readUTF();
+                        synchronized (ips) {
+                            boolean isExist = false;
+                            for(String ip : ips){
+                                if(!ip.equals(userIp)) {
+                                    outputStream.writeUTF(ip);
+                                } else {
+                                    isExist = true;
+                                }
+                            }
+                            ips.add(userIp);
+                        }
+                        socket.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
