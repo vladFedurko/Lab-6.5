c993b887e656a2b343f8e105e8f5c08ac5edbfbe
vladFedurko <fedurko.v@gmail.com>
2020-03-16 16:49:59 +0300

    ADD: Log in and registration

--- src/MainServer.java
+++ src/MainServer.java
@@ -4,34 +4,81 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 
 public class MainServer {
     private static final int SERVER_PORT = 4567;
 
-    public static void main(String[] args) {
-        ArrayList<String> ips = new ArrayList<>(5);
+    private ArrayList<User> users = new ArrayList<>(10);
+    private HashSet<UserDTO> usersDTO = new HashSet<>();
+    
+    public static void main(String[] args){
+        new MainServer(args);
+    }
+
+    private void addCleaner() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!Thread.interrupted()) {
+                    try {
+                        Thread.sleep(30000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized(usersDTO) {
+                        for(UserDTO u : usersDTO) {
+                            if(System.currentTimeMillis() - u.getLastCheck().getTime() < 30000) {
+                                usersDTO.remove(u);
+                            }
+                        }
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public MainServer(String[] args) {
+        addCleaner();
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    final ServerSocket serverSocket =
-                            new ServerSocket(SERVER_PORT);
+                    final ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
                     while(!Thread.interrupted()){
                         Socket socket = serverSocket.accept();
+                        String userIp = socket.getInetAddress().getHostAddress();
+                        
                         DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-                        DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-                        String userIp = inputStream.readUTF();
-                        synchronized (ips) {
-                            boolean isExist = false;
-                            for(String ip : ips){
-                                if(!ip.equals(userIp)) {
-                                    outputStream.writeUTF(ip);
-                                } else {
-                                    isExist = true;
+                        String state = inputStream.readUTF();
+                        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                        
+                        if (state.equals("GET")) {
+                            UserDTO userDTO = getIfOnline(userIp);
+                            if (userDTO == null) {
+                                out.writeUTF("ERROR: Firstly you need to log in");
+                            } else
+                            {
+                                userDTO.setLastCheck(new Date());
+                                out.writeUTF("OK");
+                                sendUsersInform(out, userIp);
+                            }
+                        } else {
+                            String name = inputStream.readUTF();
+                            String password = inputStream.readUTF();
+                            User user = findByName(name);
+                            if (state.equals("LOGIN")) {
+                                toLogIn(user, userIp, password, out);
+                            } else 
+                            {
+                                if (state.equals("REGISTRATION")) {
+                                    toRegister(user, name, password, out);
                                 }
                             }
-                            ips.add(userIp);
                         }
+                        out.close();
+                        inputStream.close();
                         socket.close();
                     }
                 } catch (IOException e) {
@@ -39,5 +86,67 @@ public class MainServer {
                 }
             }
         }).start();
+    }
+
+    private void toRegister(User user, String name, String password, DataOutputStream out) throws IOException {
+        if (user != null) {
+            out.writeUTF("ERROR: This user already exist");
+        } else
+        {
+            User u = new User(name, password);
+            users.add(u);
+            out.writeUTF("OK");
+        }
+    }
+
+    private void sendUsersInform (DataOutputStream out, String userIp) throws IOException {
+        synchronized (usersDTO) {
+            for (UserDTO userDTO : usersDTO) {
+                if (!userDTO.getIp().equals(userIp)) {
+                    out.writeUTF(userDTO.getIp());
+                    out.writeUTF(userDTO.getName());
+                }
+            }
+        }
+    }
+    
+    private void toLogIn (User user, String ip, String password, DataOutputStream out) throws IOException {
+        if(user == null) {
+            out.writeUTF("ERROR: You need registration");
+            return;
+        }
+        if(user.getPassword().equals(password)) {
+            out.writeUTF("OK");
+            UserDTO u = new UserDTO(ip, user.getName(), new Date());
+            addUserDTO(u);
+        } else
+        {
+            out.writeUTF("ERROR: Wrong password");
+        }
+    }
+
+    private void addUserDTO (UserDTO user) {
+        synchronized (usersDTO) {
+            usersDTO.add(user);
+        }
+    }
+    
+    private UserDTO getIfOnline(String ip) {
+        synchronized (usersDTO) {
+            for (UserDTO userDTO : usersDTO) {
+                if (userDTO.getIp().equals(ip)) {
+                    return userDTO;
+                }
+            }
+        }
+        return null;
+    }
+    
+    private User findByName(String name) {
+        for (User user : users) {
+            if(user.getName().equals(name))
+                return user;
+        }
+        return null;
     }
 }
